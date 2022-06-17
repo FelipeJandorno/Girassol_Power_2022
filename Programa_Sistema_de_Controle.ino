d2352a668a04b6fa1259cb771459c045fca3ff55
Felipe Figueiredo <84052051+FelipeJandorno@users.noreply.github.com>
2022-06-17 12:06:13 -0300

    Add files via upload
    Programação em relação ao sistema de controle (relés e sensor de corrente)

---
+++ Programa_Sistema_de_Controle.ino
@@ -0,0 +1,51 @@
+#include "EmonLib.h" //INCLUSÃO DE BIBLIOTECA
+
+#define correnteMin 500
+#define Corrente_CAL 18.40 //VALOR DE CALIBRAÇÃO (DEVE SER AJUSTADO EM PARALELO COM UM MULTÍMETRO MEDINDO A CORRENTE DA CARGA)
+const int pinoSensor = A2; //PINO ANALÓGICO EM QUE O SENSOR ESTÁ CONECTADO
+float ruido = 0.08; //RUÍDO PRODUZIDO NA SAÍDA DO SENSOR (DEVE SER AJUSTADO COM A CARGA DESLIGADA APÓS CARREGAMENTO DO CÓDIGO NO ARDUINO)
+
+EnergyMonitor emon1; //CRIA UMA INSTÂNCIA
+
+//Porta ligada ao pino IN1 do modulo
+int porta_rele1 = 7;
+//Porta ligada ao pino IN2 do modulo
+int porta_rele2 = 8;
+//Porta ligada ao pino IN2 do modulo
+int porta_rele3 = 9;  
+
+void setup(){  
+  
+  //Define pinos para o rele como saida
+  pinMode(porta_rele1, OUTPUT); 
+  pinMode(porta_rele2, OUTPUT);
+  pinMode(porta_rele3, OUTPUT);
+
+  //SENSOR DE CORRENTE
+  Serial.begin(9600); //INICIALIZA A SERIAL
+  emon1.current(pinoSensor, Corrente_CAL); //PASSA PARA A FUNÇÃO OS PARÂMETROS (PINO ANALÓGIO / VALOR DE CALIBRAÇÃO)
+}
+
+void loop(){
+  emon1.calcVI(17,100); //FUNÇÃO DE CÁLCULO (17 SEMICICLOS / TEMPO LIMITE PARA FAZER A MEDIÇÃO)
+  double corrente = emon1.Irms; //VARIÁVEL RECEBE O VALOR DE CORRENTE RMS OBTIDO
+  corrente = corrente-ruido; //VARIÁVEL RECEBE O VALOR RESULTANTE DA CORRENTE RMS MENOS O RUÍDO
+  
+  if(corrente < 0){ //SE O VALOR DA VARIÁVEL FOR MENOR QUE 0, FAZ 
+      corrente = 0; //VARIÁVEL RECEBE 0
+  }
+ 
+    Serial.print("Corrente medida: "); //IMPRIME O TEXTO NA SERIAL
+    Serial.print(corrente); //IMPRIME NA SERIAL O VALOR DE CORRENTE MEDIDA
+    Serial.println("A"); //IMPRIME O TEXTO NA SERIAL
+
+   if(corrente >= correnteMin){
+      digitalWrite(porta_rele1, LOW);  //Desliga rele 1
+      digitalWrite(porta_rele2, HIGH); //Liga rele 2
+      digitalWrite(porta_rele3, LOW); //Desliga rele 3
+   } else{
+      digitalWrite(porta_rele1, HIGH); //Liga rele 1
+      digitalWrite(porta_rele2, LOW); //Desliga rele 2
+      digitalWrite(porta_rele3, HIGH); //Liga rele 3
+   }
+}
