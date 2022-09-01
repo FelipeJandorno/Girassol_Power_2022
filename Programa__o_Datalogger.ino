d5cc63ab248f5927f0a54dc59687a995669294a4
Felipe Figueiredo <84052051+FelipeJandorno@users.noreply.github.com>
2022-09-01 16:39:40 -0300

    Add files via upload

--- Programa__o_Datalogger.ino
+++ Programa__o_Datalogger.ino
@@ -1,28 +1,22 @@
 #include <SPI.h>
 #include <SD.h>
-#include <EmonLib.h> //INCLUSÃO DE BIBLIOTECA
-
-#define Corrente_CAL 18.40 //VALOR DE CALIBRAÇÃO (DEVE SER AJUSTADO EM PARALELO COM UM MULTÍMETRO MEDINDO A CORRENTE DA CARGA)
 
 const int chipSelect = 10;
-const int pinoSensor = A2; //PINO ANALÓGICO EM QUE O SENSOR ESTÁ CONECTADO
-
-float ruido = 0.08; //RUÍDO PRODUZIDO NA SAÍDA DO SENSOR (DEVE SER AJUSTADO COM A CARGA DESLIGADA APÓS CARREGAMENTO DO CÓDIGO NO ARDUINO)
+float corrente; //A variável que vai armazenar a corrente medida pelo sensor
+float sensibilidade = 0.066;
  
 //vars do timer millis que determina o intervalo entre medidas
 unsigned long period = 60000;  //modifique aqui o intervalo entre medidas, utilize multiplos de 60000ms   
 unsigned long time_now = 0;
 int tempo = 1;
 
-EnergyMonitor emon1; //CRIA UMA INSTÂNCIA
+
+//===================================================================================//
 
 void setup() {
 
 Serial.begin(9600);
-pinMode(pinoSensor, INPUT); //DECLARA O PINO COMO ENTRADA
 
-emon1.current(pinoSensor, Corrente_CAL); //PASSA PARA A FUNÇÃO OS PARÂMETROS (PINO ANALÓGIO / VALOR DE CALIBRAÇÃO)
-   
 Serial.print(F("Lendo cartao SD"));
 delay(100);
   if (!SD.begin(chipSelect)) {
@@ -32,10 +26,12 @@ delay(100);
 Serial.println(F("Cartao SD Inicializado!"));
  
 }
- 
+
+
+//===================================================================================//
  
 void loop() {
- 
+ float corrente = calculaCorrente(filtroDaMedia());
  
   if (millis() >= time_now + period) {
   time_now += period;
@@ -45,14 +41,6 @@ void loop() {
       digitalWrite(6, LOW);        
  
  
-        emon1.calcVI(17,100); //FUNÇÃO DE CÁLCULO (17 SEMICICLOS / TEMPO LIMITE PARA FAZER A MEDIÇÃO)
-        double corrente = emon1.Irms; //VARIÁVEL RECEBE O VALOR DE CORRENTE RMS OBTIDO
-        corrente = corrente-ruido; //VARIÁVEL RECEBE O VALOR RESULTANTE DA CORRENTE RMS MENOS O RUÍDO
-  
-        if(corrente < 0){ //SE O VALOR DA VARIÁVEL FOR MENOR QUE 0, FAZ 
-          corrente = 0; //VARIÁVEL RECEBE 0
-         }
-        
         String dataString = "";
           dataString += String(corrente);
            
@@ -69,4 +57,24 @@ void loop() {
        }
        tempo++; //contador de minutos
   }       
+}
+
+
+//===================================================================================//
+
+float calculaCorrente(int sinalSensor) {
+  return (float)(sinalSensor-508)*(5.000)/(1023.000*sensibilidade);
+}
+
+
+//===================================================================================//
+
+int filtroDaMedia(){
+  long somaDasCorrentes=0, mediaDasCorrentes;
+  for(int i=0; i<1000; i++){
+    somaDasCorrentes+=analogRead(A2);
+    delay(1);
+  }
+  mediaDasrrentes=somaDasCorrentes/1000; 
+  return mediaDasCorrentesCo;
 }
